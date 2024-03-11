15da5230e25d1dc06f551eaf5e5d43b2aec89208
Charlotte Sun <suncharlotte7@gmail.com>
2024-03-11 17:10:07 -0400

    pass all the tests

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+let employee = {
+    name: "Sam",
+    streetAddress: "11 Broadway",
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const nemployee = {...employee};
+    nemployee[key] = value;
+    return nemployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    key = 'Sam';
+    value = '12 Broadway';
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const nemployee = {...employee};
+    delete nemployee[key];
+    return nemployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
